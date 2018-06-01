f3adac3f1c73b85a64d944b5b4a8812306859851
prochnowc <christian.prochnow77@gmail.com>
2018-06-01 13:08:10 +0200

    Fix linux test run.

--- test.ps1
+++ test.ps1
@@ -5,4 +5,4 @@ Param([string]$Configuration="Debug")
 
 $ArtifactsDir = Join-Path $PSScriptRoot 'artifacts'
 
-dotnet vstest ((ls -Recurse *.Tests.dll | % FullName) -Match "bin") "/Logger:trx;LogFileName=test-results.trx" /ResultsDirectory:$ArtifactsDir
+dotnet vstest ((Get-ChildItem -Recurse *.Tests.dll | % FullName) -Match "bin") "/Logger:trx;LogFileName=test-results.trx" /ResultsDirectory:$ArtifactsDir
