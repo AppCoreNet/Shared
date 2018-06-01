86a80190a38420a07183b2947b448ba2a126b7a2
prochnowc <christian.prochnow77@gmail.com>
2018-06-01 12:06:51 +0200

    Update versioning for ci-builds.

--- build.ps1
+++ build.ps1
@@ -1,8 +1,16 @@
 #!/usr/bin/env powershell
 #requires -version 4
 
+Param([string]$VersionSuffix="",[Int32]$BuildNumber=0)
+
 $ArtifactsDir = Join-Path $PSScriptRoot 'artifacts'
 
-dotnet restore
-dotnet build --no-restore
-dotnet pack --no-build --no-restore -o $ArtifactsDir
+$ExtraArgs = ""
+If ($VersionSuffix.length -gt 0)
+{
+	$ExtraArgs = "/p:VersionSuffix=$VersionSuffix{0:0000}" -f $BuildNumber
+}
+
+dotnet restore $ExtraArgs
+dotnet build --no-restore $ExtraArgs
+dotnet pack --no-build --no-restore -o $ArtifactsDir $ExtraArgs
