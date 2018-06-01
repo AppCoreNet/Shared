35a40dc3c2af2f365a89e8f9b73a2349334b9b99
prochnowc <christian.prochnow77@gmail.com>
2018-06-01 12:21:38 +0200

    Added configuration to appveyor build.ps1

--- build.ps1
+++ build.ps1
@@ -1,16 +1,23 @@
 #!/usr/bin/env powershell
 #requires -version 4
 
-Param([string]$VersionSuffix="",[Int32]$BuildNumber=0)
+Param([string]$Configuration="",[string]$VersionSuffix="",[Int32]$BuildNumber=0)
 
 $ArtifactsDir = Join-Path $PSScriptRoot 'artifacts'
 
-$ExtraArgs = ""
+$ExtraArgs = @()
+$ExtraBuildArgs = @()
+
 If ($VersionSuffix.length -gt 0)
 {
-	$ExtraArgs = "/p:VersionSuffix=$VersionSuffix{0:0000}" -f $BuildNumber
+	$ExtraArgs += "/p:VersionSuffix=$VersionSuffix{0:0000}" -f $BuildNumber
+}
+
+If ($Configuration.length -gt 0)
+{
+	$ExtraBuildArgs += "--configuration", "$Configuration"
 }
 
 dotnet restore $ExtraArgs
-dotnet build --no-restore $ExtraArgs
-dotnet pack --no-build --no-restore -o $ArtifactsDir $ExtraArgs
+dotnet build --no-restore $ExtraBuildArgs $ExtraArgs
+dotnet pack --no-build --no-restore $ExtraBuildArgs -o $ArtifactsDir $ExtraArgs
