46fda62cf2f52fc2159e40a0e8d6dbb8d74b7b11
Christian Prochnow <christian.prochnow77@gmail.com>
2018-06-02 10:27:00 +0200

    Improve CI build. (#5)

--- test.ps1
+++ test.ps1
@@ -3,6 +3,8 @@
 
 Param([string]$Configuration="Debug")
 
+$ErrorActionPreference = "Stop"
 $ArtifactsDir = Join-Path $PSScriptRoot 'artifacts'
 
 dotnet vstest ((Get-ChildItem -Recurse *.Tests.dll | % FullName) -Match "bin") "/Logger:trx;LogFileName=test-results.trx" /ResultsDirectory:$ArtifactsDir
+If ($LastExitCode -ne 0) { throw "Tests failed." }
