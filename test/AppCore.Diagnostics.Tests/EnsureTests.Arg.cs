7a27172f26d25790d12c9a79d1095f8b875dd080
prochnowc <christian.prochnow77@gmail.com>
2018-06-03 23:24:27 +0200

    Added more unit tests.

--- test/AppCore.Diagnostics.Tests/EnsureTests.Arg.cs
+++ test/AppCore.Diagnostics.Tests/EnsureTests.Arg.cs
@@ -30,6 +30,112 @@ namespace AppCore.Diagnostics
             {
             }
 
+            [Fact]
+            public void NotNullThrowsWhenNull()
+            {
+                var exception = Assert.Throws<ArgumentNullException>(
+                    () => Ensure.Arg.NotNull((string)null, "param"));
+
+                exception.ParamName.Should()
+                         .Be("param");
+            }
+
+            [Fact]
+            public void NotEmptyThrowsWhenNull()
+            {
+                var exception = Assert.Throws<ArgumentNullException>(
+                    () => Ensure.Arg.NotEmpty(null, "param"));
+
+                exception.ParamName.Should()
+                         .Be("param");
+            }
+
+            [InlineData("")]
+            [InlineData("   ")]
+            [Theory]
+            public void NotEmptyThrowsWhenEmptyOrWhitespace(string value)
+            {
+                var exception = Assert.Throws<ArgumentException>(
+                    () => Ensure.Arg.NotEmptyButNull(value, "param"));
+
+                exception.ParamName.Should()
+                         .Be("param");
+            }
+
+            [InlineData("")]
+            [InlineData("   ")]
+            [Theory]
+            public void NotEmptyButNullThrowsWhenEmptyOrWhitespace(string value)
+            {
+                var exception = Assert.Throws<ArgumentException>(
+                    () => Ensure.Arg.NotEmpty(value, "param"));
+
+                exception.ParamName.Should()
+                         .Be("param");
+            }
+
+
+            [Fact]
+            public void NotEmptyThrowsForNullCollection()
+            {
+                var exception = Assert.Throws<ArgumentNullException>(
+                    () => Ensure.Arg.NotEmpty((IReadOnlyCollection<string>)null, "param"));
+
+                exception.ParamName.Should()
+                         .Be("param");
+            }
+
+            [Fact]
+            public void NotEmptyThrowsForEmptyCollection()
+            {
+                var exception = Assert.Throws<ArgumentException>(
+                    () => Ensure.Arg.NotEmpty(new List<string>(), "param"));
+
+                exception.ParamName.Should()
+                         .Be("param");
+            }
+
+            [Theory]
+            [InlineData(-1)]
+            [InlineData(11)]
+            public void InRangeThrowsWhenValueExceedsRange(int value)
+            {
+                var exception = Assert.Throws<ArgumentOutOfRangeException>(
+                    () => Ensure.Arg.InRange(value, 0, 10, "param"));
+
+                exception.ParamName.Should()
+                         .Be("param");
+
+                exception.ActualValue.Should()
+                         .Be(value);
+            }
+
+            [Fact]
+            public void MaxLengthThrowsForLongerString()
+            {
+                var exception = Assert.Throws<ArgumentOutOfRangeException>(
+                    () => Ensure.Arg.MaxLength(" ", 0, "param"));
+
+                exception.ParamName.Should()
+                         .Be("param");
+
+                exception.ActualValue.Should()
+                         .Be(1);
+            }
+
+            [Fact]
+            public void MinLengthThrowsForShorterString()
+            {
+                var exception = Assert.Throws<ArgumentOutOfRangeException>(
+                    () => Ensure.Arg.MinLength("", 1, "param"));
+
+                exception.ParamName.Should()
+                         .Be("param");
+
+                exception.ActualValue.Should()
+                         .Be(0);
+            }
+
             [Theory]
             [InlineData(typeof(IEnumerable))]
             [InlineData(typeof(IEnumerable<string>))]
