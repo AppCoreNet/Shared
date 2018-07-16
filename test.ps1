73c584a949d9f1932b5d5e0ba4058984d2ac4b15
prochnowc <christian.prochnow77@gmail.com>
2018-07-16 11:45:51 +0200

    Added net452 as target framework.

--- test.ps1
+++ test.ps1
@@ -6,5 +6,10 @@ Param([string]$Configuration="Debug")
 $ErrorActionPreference = "Stop"
 $ArtifactsDir = Join-Path $PSScriptRoot 'artifacts'
 
-dotnet vstest ((Get-ChildItem -Recurse *.Tests.dll | % FullName) -Match "bin") "/Logger:trx;LogFileName=test-results.trx" /ResultsDirectory:$ArtifactsDir
+dotnet vstest ((Get-ChildItem -Recurse *.Tests.dll | % FullName) -NotMatch "obj" -Match "netcoreapp") "/Framework:.NETCoreApp,Version=v2.0" "/Logger:trx;LogFileName=test-results-netcore.trx" /ResultsDirectory:$ArtifactsDir
 If ($LastExitCode -ne 0) { throw "Tests failed." }
+
+If ($env:CI_LINUX -ne "true") {
+  dotnet vstest ((Get-ChildItem -Recurse *.Tests.dll | % FullName) -NotMatch "obj" -Match "net452") "/Framework:.NETFramework,Version=v4.5.2" "/Logger:trx;LogFileName=test-results-net452.trx" /ResultsDirectory:$ArtifactsDir
+  If ($LastExitCode -ne 0) { throw "Tests failed." }
+}
