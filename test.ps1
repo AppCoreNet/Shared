03e0d18282fdd4861854ecc2ba86a96cefd1940d
prochnowc <christian.prochnow77@gmail.com>
2018-10-31 08:59:31 +0100

    Updated test sdk.

--- test.ps1
+++ test.ps1
@@ -5,11 +5,11 @@ Param([string]$Configuration="Debug")
 
 $ErrorActionPreference = "Stop"
 $ArtifactsDir = Join-Path $PSScriptRoot 'artifacts'
+$TestResultsDir = Join-Path $ArtifactsDir 'tests'
 
-dotnet vstest ((Get-ChildItem -Recurse *.Tests.dll | % FullName) -NotMatch "obj" -Match "netcoreapp") "/Framework:.NETCoreApp,Version=v2.0" "/Logger:trx;LogFileName=test-results-netcore.trx" /ResultsDirectory:$ArtifactsDir
-If ($LastExitCode -ne 0) { throw "Tests failed." }
-
-If ($env:CI_LINUX -ne "true") {
-  dotnet vstest ((Get-ChildItem -Recurse *.Tests.dll | % FullName) -NotMatch "obj" -Match "net452") "/Framework:.NETFramework,Version=v4.5.2" "/Logger:trx;LogFileName=test-results-net452.trx" /ResultsDirectory:$ArtifactsDir
-  If ($LastExitCode -ne 0) { throw "Tests failed." }
+Get-ChildItem -Path test -Recurse *.csproj | % {
+  dotnet test "$($_.FullName)" -c "$Configuration" --collect "Code Coverage" -l trx -r "$TestResultsDir"
+  $TestsFailed = $TestsFailed -or ($LastExitCode -ne 0)
 }
+
+If ($TestsFailed) { throw "Some tests failed." }
