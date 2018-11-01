d13544cef561d071d08d2d08bb613c3ff174a08d
prochnowc <christian.prochnow77@gmail.com>
2018-11-01 06:27:47 +0100

    Reverted to vstest.

--- test.ps1
+++ test.ps1
@@ -7,7 +7,22 @@ $ErrorActionPreference = "Stop"
 $ArtifactsDir = Join-Path $PSScriptRoot 'artifacts'
 $TestResultsDir = Join-Path $ArtifactsDir 'tests'
 
-Get-ChildItem -Path test -Recurse *.csproj | % {
-  dotnet test "$($_.FullName)" -c "$Configuration" --no-build -l trx -r "$TestResultsDir"
-  $TestsFailed = $TestsFailed -or ($LastExitCode -ne 0)
+$TargetFrameworks = @{
+  "netcoreapp1.0" = ".NETCoreApp,Version=v1.0"
+  "netcoreapp1.1" = ".NETCoreApp,Version=v1.1"
+  "netcoreapp2.0" = ".NETCoreApp,Version=v2.0"
+  "netcoreapp2.1" = ".NETCoreApp,Version=v2.1"
+}
+
+If (!$IsLinux) {
+  $TargetFrameworks["net452"] = ".NETFramework,Version=v4.5.2"
+}
+
+ForEach ($TargetFramework in $TargetFrameworks.GetEnumerator()) {
+  $AssemblyPath = Join-Path bin $Configuration | Join-Path -ChildPath $TargetFramework.Key
+  $Assemblies = ((Get-ChildItem -Recurse *.Tests.dll | % FullName) -Match [regex]::Escape($AssemblyPath))
+  If ($Assemblies.Length -gt 0) {
+    dotnet vstest $Assemblies "/Framework:$($TargetFramework.Value)" "/Logger:trx;LogFileName=testresults-$($TargetFramework.Key).trx" /ResultsDirectory:$TestResultsDir
+    $TestsFailed = $TestsFailed -or ($LastExitCode -ne 0)
+  }
 }
